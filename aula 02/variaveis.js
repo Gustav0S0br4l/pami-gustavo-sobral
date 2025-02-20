148d895a551d9a365cc04434c9f5dd86da4cf0d1
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-02-20 08:28:04 -0300

    Aula sobre tipo de Variaveis

---
+++ aula 02/variaveis.js
@@ -0,0 +1,28 @@
+var numero = 2;
+let texto = "hello";
+const oQueEIsso = "?";
+// o ponto e virgula é opcional: Boas Práticas o usam
+
+console.log("Tipo var: ", numero);
+// concatenar com virgula me garante um espaço
+
+numero = 13;
+console.log(texto +" O numero agora é "+ numero);
+console.log(texto ,"O numero agora é", numero);
+console.log(`${texto} O numero agora é ${numero}`);
+
+
+texto = 69 //SEXO HEHE
+
+console.log(texto + numero);
+// concatenar com + dois valores numericos vai 
+// acabar somando
+
+console.log(oQueQIsso)
+oQueEIsso = "!"
+console.log(oQueEIsso)
+// constantes não podem ter novos valores assinalados a ela
+
+// oQueEIsso = "Uma constante????????????"
+
+// console.log("oQueEIsso")
